c4e9e86ee016b752b52bc0864f2f4c3501836000
Pavel Maslyaev <pavel-max@rambler.ru>
2016-08-04 17:16:36 +0300

    refactoring

--- app/src/main/java/com/next/myforismatic/ui/presenters/QuotesListPresenterImpl.java
+++ app/src/main/java/com/next/myforismatic/ui/presenters/QuotesListPresenterImpl.java
@@ -1,12 +1,7 @@
 package com.next.myforismatic.ui.presenters;
 
-import android.content.BroadcastReceiver;
 import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.support.annotation.NonNull;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,7 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import com.next.myforismatic.R;
 import com.next.myforismatic.models.Quote;
 import com.next.myforismatic.providers.QuoteContentProvider;
-import com.next.myforismatic.services.ForismaticIntentService;
 import com.next.myforismatic.ui.common.QuoteListAdapter;
 import com.next.myforismatic.ui.contracts.QuoteListContract;
 import com.next.myforismatic.ui.quoteslist.QuoteListFragment;
@@ -43,22 +37,10 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
     private RecyclerView recyclerView;
 
     private QuoteListFragment fragment;
-    private QuoteListContract.QuoteListView view;
-
-    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getStringExtra("message").equals("finish")) {
-                restartLoader();
-                LocalBroadcastManager.getInstance(fragment.getContext()).unregisterReceiver(messageReceiver);
-            }
-        }
-    };
 
     @Override
     public void viewCreated(QuoteListContract.QuoteListView view) {
         fragment = (QuoteListFragment)view;
-        this.view = view;
 
         recyclerView = (RecyclerView) fragment.getActivity().findViewById(R.id.fragment_quote_list_rv);
 
@@ -70,19 +52,19 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setColorSchemeColors(R.color.blue, R.color.green, R.color.yellow, R.color.red);
 
-        swipeRefreshLayout.postDelayed(() -> {
-            if (ForismaticIntentService.isLoading) {
-                registerReceiver();
-                swipeRefreshLayout.setRefreshing(true);
-            }
-        }, 500);
+        loadData();
+    }
 
+    @Override
+    public void loadData()
+    {
         getQuotes();
     }
 
     @Override
-    public void loadData() {
-        registerReceiverAndStartService();
+    public void onRefresh() {
+        swipeRefreshLayout.setRefreshing(true);
+        loadData();
     }
 
     @Override
@@ -90,45 +72,16 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
         if (subscription != null) {
             subscription.unsubscribe();
         }
-
-        LocalBroadcastManager.getInstance(fragment.getContext()).unregisterReceiver(messageReceiver);
-    }
-
-    private void restartLoader() {
-        if (!swipeRefreshLayout.isRefreshing()) {
-            swipeRefreshLayout.setRefreshing(true);
-        }
-        //getQuotesInternet();
-        getQuotes();
-    }
-
-    private void registerReceiverAndStartService() {
-        registerReceiver();
-        //getQuotesFromInternetService();
-        getQuotes();
-    }
-/*
-    private void getQuotesFromInternetService() {
-        Activity activity = fragment.getActivity();
-        Intent intent = new Intent(activity, ForismaticIntentService.class).putExtra("size", QUOTES_SIZE);
-        activity.startService(intent);
-    }
-*/
-    private void registerReceiver() {
-        LocalBroadcastManager.getInstance(fragment.getContext()).registerReceiver(
-                messageReceiver,
-                new IntentFilter("endDownload")
-        );
     }
 
     private void getQuotes() {
         if (adapter.getItemCount() == 0) {
             getQuotesDb();
-        } else {
-            getQuotesInternet();
-        }
 
-        if (adapter.getItemCount() == 0) {
+            if (adapter.getItemCount() == 0) {
+                getQuotesInternet();
+            }
+        } else {
             getQuotesInternet();
         }
     }
@@ -156,7 +109,7 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
         List<Quote> quotes = new ArrayList<>(size);
         try {
             for (int i = 0; i < size; i++) {
-                Call<Quote> callQ = getQuote(i);
+                Call<Quote> callQ = getQuoteFromInternet(i);
                 Quote quote = callQ.execute().body();
                 quotes.add(quote);
             }
@@ -164,6 +117,10 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
             //not supported
         }
         return quotes;
+    }
+
+    private Call<Quote> getQuoteFromInternet(int key) {
+        return fragment.getForismaticService().getQuote("getQuote", "json", "ru", key);
     }
 
     private void getQuotesDb() {
@@ -189,10 +146,6 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
                         QuoteContentProvider.QUOTE_ID));
     }
 
-    private Call<Quote> getQuote(int key) {
-        return fragment.getForismaticService().getQuote("getQuote", "json", "ru", key);
-    }
-
     private void insertQuotes(@NonNull List<Quote> quotes) {
         for (Quote quote : quotes) {
             ContentValues contentValues = quote.getContentValues();
@@ -206,11 +159,4 @@ public class QuotesListPresenterImpl implements QuoteListContract.Presenter, Swi
         swipeRefreshLayout.setRefreshing(false);
         adapter.setQuotes(quotes);
     }
-
-    @Override
-    public void onRefresh() {
-        swipeRefreshLayout.setRefreshing(true);
-        registerReceiverAndStartService();
-    }
-
 }
