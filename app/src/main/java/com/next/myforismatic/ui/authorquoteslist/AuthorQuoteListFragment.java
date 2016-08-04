097d32e6649d45f1ac95a265bd595826eb3a9387
Pavel Maslyaev <pavel-max@rambler.ru>
2016-08-04 18:00:31 +0300

    second part of MVC

--- app/src/main/java/com/next/myforismatic/ui/authorquoteslist/AuthorQuoteListFragment.java
+++ app/src/main/java/com/next/myforismatic/ui/authorquoteslist/AuthorQuoteListFragment.java
@@ -1,18 +1,9 @@
 package com.next.myforismatic.ui.authorquoteslist;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,21 +11,19 @@ import android.view.ViewGroup;
 
 import com.next.myforismatic.R;
 import com.next.myforismatic.models.Quote;
-import com.next.myforismatic.providers.QuoteContentProvider;
 import com.next.myforismatic.ui.base.BaseFragment;
-import com.next.myforismatic.ui.common.QuoteListAdapter;
-import com.next.myforismatic.utils.CursorParse;
+import com.next.myforismatic.ui.contracts.QuoteListContract;
+import com.next.myforismatic.ui.presenters.AuthorQuoteListPresenterImpl;
 
 import java.util.List;
 
 /**
  * Created by maslparu on 06.07.2016.
  */
-public class AuthorQuoteListFragment extends BaseFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class AuthorQuoteListFragment extends BaseFragment implements QuoteListContract.QuoteListView {
 
-    private RecyclerView recyclerView;
-    private QuoteListAdapter adapter;
     private String author;
+    private QuoteListContract.Presenter presenter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,37 +33,18 @@ public class AuthorQuoteListFragment extends BaseFragment implements LoaderManag
 
         ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        presenter = new AuthorQuoteListPresenterImpl();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_author_quote_list, container, false);
-        recyclerView = (RecyclerView) v.findViewById(R.id.fragment_author_quote_list_rv);
+
+        presenter.viewCreated(this);
+
         return v;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        adapter = new QuoteListAdapter();
-        adapter.setAuthorQuotes(true);
-        recyclerView.setAdapter(adapter);
-
-        Bundle bundle = this.getArguments();
-
-        if (bundle != null) {
-            author = bundle.getString(QuoteContentProvider.QUOTE_AUTHOR);
-            getActivity().setTitle(author);
-        }
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        getLoaderManager().restartLoader(R.id.quote_cursor_loader, null, this);
     }
 
     @Override
@@ -89,47 +59,14 @@ public class AuthorQuoteListFragment extends BaseFragment implements LoaderManag
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if (id == R.id.quote_cursor_loader) {
-            return new MyCursorLoader(getContext(), QuoteContentProvider.QUOTE_CONTENT_URI, author);
-        }
-        return null;
+    public void onDestroy() {
+        presenter.viewDestroyed();
+
+        super.onDestroy();
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        List<Quote> quotes = CursorParse.parseQuotes(data);
-        setQuotes(quotes);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
-    }
-
-    private void setQuotes(@NonNull List<Quote> quotes) {
-        adapter.setQuotes(quotes);
-    }
-
-
-    private static class MyCursorLoader extends CursorLoader {
-
-        private Uri uri;
-        private String author;
-
-        public MyCursorLoader(Context context, Uri uri, String author) {
-            super(context);
-            this.uri = uri;
-            this.author = author.replaceAll(" \u00a9", "");
-        }
-
-        @Override
-        public Cursor loadInBackground() {
-            return getContext().getContentResolver().query(
-                    uri, null, QuoteContentProvider.QUOTE_AUTHOR, new String[] { author },
-                    QuoteContentProvider.QUOTE_ID
-            );
-        }
-
+    public void setData(List<Quote> quotes) {
+        presenter.loadData();
     }
 }
---
+++ app/src/main/java/com/next/myforismatic/ui/presenters/AuthorQuoteListPresenterImpl.java
@@ -0,0 +1,89 @@
+package com.next.myforismatic.ui.presenters;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import com.next.myforismatic.R;
+import com.next.myforismatic.models.Quote;
+import com.next.myforismatic.providers.QuoteContentProvider;
+import com.next.myforismatic.ui.common.QuoteListAdapter;
+import com.next.myforismatic.ui.contracts.QuoteListContract;
+import com.next.myforismatic.utils.CursorParse;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by maslparu on 04.08.2016.
+ */
+public class AuthorQuoteListPresenterImpl implements QuoteListContract.Presenter {
+
+    private Fragment fragment;
+
+    private Subscription subscription;
+    private RecyclerView recyclerView;
+    private QuoteListAdapter adapter;
+
+    private String author;
+
+    @Override
+    public void viewCreated(QuoteListContract.QuoteListView view) {
+        fragment = (Fragment) view;
+
+        Bundle bundle = fragment.getArguments();
+
+        if (bundle != null) {
+            author = bundle.getString(QuoteContentProvider.QUOTE_AUTHOR);
+            fragment.getActivity().setTitle(author);
+        }
+
+        recyclerView = (RecyclerView) fragment.getActivity().findViewById(R.id.fragment_author_quote_list_rv);
+
+        adapter = new QuoteListAdapter();
+        adapter.setAuthorQuotes(true);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void loadData() {
+        subscription = observeQuotesByAuthor()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::setData);
+    }
+
+    private Observable<List<Quote>> observeQuotesByAuthor() {
+        return Observable.fromCallable(() -> {
+            List<Quote> quotes = getQuotesByAuthor();
+            return quotes;
+        }).subscribeOn(Schedulers.io());
+    }
+
+    @NonNull
+    private List<Quote> getQuotesByAuthor() {
+        return CursorParse.parseQuotes(
+                fragment.getActivity().getContentResolver().query(
+                        QuoteContentProvider.QUOTE_CONTENT_URI,
+                        null,
+                        QuoteContentProvider.QUOTE_AUTHOR,
+                        new String[] { author },
+                        QuoteContentProvider.QUOTE_ID
+                ));
+    }
+
+    @Override
+    public void viewDestroyed() {
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
+    }
+
+    private void setData(List<Quote> quotes) {
+        adapter.setQuotes(quotes);
+    }
+}
